018b225102e78f3925ba2acd9a44f405a1f52b36
Ariel Ignacio Chacón Medel <medel.ariel@gmail.com>
2022-09-26 19:03:09 -0300

    correccion

--- src/main/java/com/duocuc/turismoreal/presentation/UsuarioController.java
+++ src/main/java/com/duocuc/turismoreal/presentation/UsuarioController.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.duocuc.turismoreal.request.ActualizarUsuario;
 import com.duocuc.turismoreal.response.UsuarioResponse;
 import com.duocuc.turismoreal.service.UsuarioService;
 
+@RestController
+@RequestMapping("usuario")
 public class UsuarioController {
 
     @Autowired
